5cfa60db2b424af6722fa70acd5742311a00747a
scrKevin <scrKevin@github.com>
2021-04-14 22:09:33 +0200

    webcam fix

--- game_modules/client_side/client-side.js
+++ game_modules/client_side/client-side.js
@@ -411,24 +411,34 @@ $( document ).ready(function() {
     })
   }
 
-  navigator.mediaDevices.getUserMedia({video: {
-                          width: {
-                              max: 320,
-                              ideal: 160 
-                          },
-                          height: {
-                              max: 240,
-                              ideal: 120
-                          }
-                      }, audio: true})
-    .then(function(stream) {
-      myStream = stream;
-      InitWebSocket();
-      $('#welcomeModal').modal({
-                          show: true,
-                          backdrop: 'static',
-                          keyboard: false
-                          });
+  // navigator.mediaDevices.getUserMedia({video: {
+  //                         width: {
+  //                             max: 320,
+  //                             ideal: 160 
+  //                         },
+  //                         height: {
+  //                             max: 240,
+  //                             ideal: 120
+  //                         }
+  //                     }, audio: true})
+  //   .then(function(stream) {
+  //     myStream = stream;
+  //     InitWebSocket();
+  //     $('#welcomeModal').modal({
+  //                         show: true,
+  //                         backdrop: 'static',
+  //                         keyboard: false
+  //                         });
+  // });
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+  .then(function(stream) {
+  myStream = stream;
+  InitWebSocket();
+  $('#welcomeModal').modal({
+      show: true,
+      backdrop: 'static',
+      keyboard: false
+      });
   });
 });
 
