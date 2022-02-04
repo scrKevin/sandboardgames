d615e283eb98d428852e8b3e321b9251806806e4
scrKevin <scrKevin@github.com>
2022-02-04 11:18:30 +0100

    disable default ice servers

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -16645,8 +16645,8 @@ Peer.config = {
   iceServers: [
     {
       urls: [
-        'stun:stun.l.google.com:19302',
-        'stun:global.stun.twilio.com:3478'
+        // 'stun:stun.l.google.com:19302',
+        // 'stun:global.stun.twilio.com:3478'
       ]
     }
   ],
